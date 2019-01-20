4e4b74fa95bd504dceeb17090086a58a0c9accd6
Alex Patterson <admin@ajonp.com>
2019-01-20 15:15:12 -0500

    keep subscriptions cleaned up

--- src/app/modules/books/book-detail/book-detail.component.ts
+++ src/app/modules/books/book-detail/book-detail.component.ts
@@ -1,7 +1,7 @@
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { FirestoreService } from 'src/app/core/services/firestore.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Book } from 'src/app/core/models/book';
 import { Chapter } from 'src/app/core/models/chapter';
 import { Section } from 'src/app/core/models/section';
@@ -11,34 +11,44 @@ import { Section } from 'src/app/core/models/section';
   templateUrl: './book-detail.component.html',
   styleUrls: ['./book-detail.component.scss']
 })
-export class BookDetailComponent implements OnInit {
+export class BookDetailComponent implements OnInit, OnDestroy {
   book$: Observable<Book>;
   chapter$: Observable<Chapter>;
   section$: Observable<Section>;
   bookId: string;
   chapterId: string;
   sectionId: string;
+  subscriptions: Subscription[] = [];
   constructor(private fs: FirestoreService, private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.route.paramMap.subscribe(paramMap => {
-      this.bookId = paramMap.get('bookId');
-      this.book$ = this.fs.getBook(this.bookId);
-    });
-    this.route.queryParamMap.subscribe(paramMap => {
-      this.chapterId = paramMap.get('chapterId');
-      this.sectionId = paramMap.get('sectionId');
+    this.subscriptions.push(
+      this.route.paramMap.subscribe(paramMap => {
+        this.bookId = paramMap.get('bookId');
+        this.book$ = this.fs.getBook(this.bookId);
+      })
+    );
+    this.subscriptions.push(
+      this.route.queryParamMap.subscribe(paramMap => {
+        this.chapterId = paramMap.get('chapterId');
+        this.sectionId = paramMap.get('sectionId');
 
-      if (this.bookId && this.chapterId) {
-        this.chapter$ = this.fs.getBookChapter(this.bookId, this.chapterId);
-      }
-      if (this.bookId && this.chapterId && this.sectionId) {
-        this.section$ = this.fs.getBookSection(
-          this.bookId,
-          this.chapterId,
-          this.sectionId
-        );
-      }
+        if (this.bookId && this.chapterId) {
+          this.chapter$ = this.fs.getBookChapter(this.bookId, this.chapterId);
+        }
+        if (this.bookId && this.chapterId && this.sectionId) {
+          this.section$ = this.fs.getBookSection(
+            this.bookId,
+            this.chapterId,
+            this.sectionId
+          );
+        }
+      })
+    );
+  }
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => {
+      s.unsubscribe();
     });
   }
 }
--- src/app/modules/books/book-tree/book-tree.component.ts
+++ src/app/modules/books/book-tree/book-tree.component.ts
@@ -1,6 +1,6 @@
 import { Book } from 'src/app/core/models/book';
-import { Injectable, Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, merge } from 'rxjs';
+import { Injectable, Component, OnInit, OnDestroy } from '@angular/core';
+import { BehaviorSubject, Observable, merge, Subscription } from 'rxjs';
 import { FlatTreeControl } from '@angular/cdk/tree';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FirestoreService } from 'src/app/core/services/firestore.service';
@@ -26,6 +26,7 @@ export class DynamicFlatNode {
 export class DynamicDataSource {
   dataChange = new BehaviorSubject<DynamicFlatNode[]>([]);
   bookTree = {};
+  subscriptions: Subscription[] = [];
   get data(): DynamicFlatNode[] {
     return this.dataChange.value;
   }
@@ -41,29 +42,33 @@ export class DynamicDataSource {
     private router: Router
   ) {
     /** Initial data from database */
-    this.route.queryParams.subscribe(params => {
-      console.log(params);
-    });
-    this.route.paramMap.subscribe(paramMap => {
-      const bookId = paramMap.get('bookId');
-      this.fs.getBookChapters(bookId).subscribe(chapters => {
-        const nodes: DynamicFlatNode[] = [];
-        chapters.sort((a, b) => (a.sort < b.sort ? -1 : 1));
-        chapters.forEach(chapter =>
-          nodes.push(
-            new DynamicFlatNode(
-              chapter.title,
-              0,
-              true,
-              false,
-              { id: bookId },
-              chapter
+    this.subscriptions.push(
+      this.route.queryParams.subscribe(params => {
+        console.log(params);
+      })
+    );
+    this.subscriptions.push(
+      this.route.paramMap.subscribe(paramMap => {
+        const bookId = paramMap.get('bookId');
+        this.fs.getBookChapters(bookId).subscribe(chapters => {
+          const nodes: DynamicFlatNode[] = [];
+          chapters.sort((a, b) => (a.sort < b.sort ? -1 : 1));
+          chapters.forEach(chapter =>
+            nodes.push(
+              new DynamicFlatNode(
+                chapter.title,
+                0,
+                true,
+                false,
+                { id: bookId },
+                chapter
+              )
             )
-          )
-        );
-        this.data = nodes;
-      });
-    });
+          );
+          this.data = nodes;
+        });
+      })
+    );
   }
 
   connect(collectionViewer: CollectionViewer): Observable<DynamicFlatNode[]> {
@@ -101,43 +106,45 @@ export class DynamicDataSource {
     const index = this.data.indexOf(node);
     node.isLoading = true;
     if (expand) {
-      this.fs
-        .getBookSections(node.book.id, node.chapter.id)
-        .subscribe(async sections => {
-          console.log(sections);
-          const nodes: DynamicFlatNode[] = [];
-          sections.sort((a, b) => (a.sort < b.sort ? -1 : 1));
-          sections.forEach(section =>
-            nodes.push(
-              new DynamicFlatNode(
-                section.title,
-                1,
-                false,
-                false,
-                node.book,
-                node.chapter,
-                section
+      this.subscriptions.push(
+        this.fs
+          .getBookSections(node.book.id, node.chapter.id)
+          .subscribe(async sections => {
+            console.log(sections);
+            const nodes: DynamicFlatNode[] = [];
+            sections.sort((a, b) => (a.sort < b.sort ? -1 : 1));
+            sections.forEach(section =>
+              nodes.push(
+                new DynamicFlatNode(
+                  section.title,
+                  1,
+                  false,
+                  false,
+                  node.book,
+                  node.chapter,
+                  section
+                )
               )
-            )
-          );
-          this.data.splice(index + 1, 0, ...nodes);
-          this.dataChange.next(this.data);
+            );
+            this.data.splice(index + 1, 0, ...nodes);
+            this.dataChange.next(this.data);
 
-          // Update query params on current chapter
-          await this.router.navigate([], {
-            relativeTo: this.route,
-            queryParams: { chapterId: node.chapter.id },
-            queryParamsHandling: 'merge'
-          });
-          // Remove any left over section params
-          await this.router.navigate([], {
-            relativeTo: this.route,
-            queryParams: { sectionId: '' },
-            queryParamsHandling: 'merge'
-          });
+            // Update query params on current chapter
+            await this.router.navigate([], {
+              relativeTo: this.route,
+              queryParams: { chapterId: node.chapter.id },
+              queryParamsHandling: 'merge'
+            });
+            // Remove any left over section params
+            await this.router.navigate([], {
+              relativeTo: this.route,
+              queryParams: { sectionId: '' },
+              queryParamsHandling: 'merge'
+            });
 
-          node.isLoading = false;
-        });
+            node.isLoading = false;
+          })
+      );
     } else {
       let count = 0;
       for (
@@ -158,7 +165,7 @@ export class DynamicDataSource {
   templateUrl: './book-tree.component.html',
   styleUrls: ['./book-tree.component.scss']
 })
-export class BookTreeComponent implements OnInit {
+export class BookTreeComponent implements OnInit, OnDestroy {
   treeControl: FlatTreeControl<DynamicFlatNode>;
   dataSource: DynamicDataSource;
   constructor(
@@ -179,6 +186,12 @@ export class BookTreeComponent implements OnInit {
   }
 
   ngOnInit() {}
+
+  ngOnDestroy() {
+    this.dataSource.subscriptions.forEach(s => {
+      s.unsubscribe();
+    });
+  }
 
   section(node: DynamicFlatNode) {
     // Update query params on current chapter
