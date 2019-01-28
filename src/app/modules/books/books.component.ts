3a7c15204ed4c1ae9ddcfbe9bb242e174a0ca3c1
Alex Patterson <admin@ajonp.com>
2019-01-28 14:49:05 -0500

    add book logic for router use

--- src/app/modules/books/books.component.ts
+++ src/app/modules/books/books.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-books',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books.component.scss']
 })
 export class BooksComponent implements OnInit {
+  showBooksDrawer$ = new BehaviorSubject(false);
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onBooksDrawerActivate(e) {
+    this.showBooksDrawer$.next(true);
   }
-
+  onBooksDrawerDeactivate(e) {
+    this.showBooksDrawer$.next(false);
+  }
 }
